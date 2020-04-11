14b07073f5c446b6b09867e998fc3f4ca612721c
marinasaf777 <myjob.front.end@gmail.com>
2020-04-12 00:57:42 +0300

    learn work with input

--- todolist/js/index.js
+++ todolist/js/index.js
@@ -1,38 +1,8 @@
 "use strict";
+let input = document.getElementById('text');
+let btn = document.getElementById('btn');
 
-/*
-let Mass = ["jas", 'lll', '1', 2, 3];
-let st = true;
+btn.addEventListener('click', function(){
+    console.log(input.value);
+});
 
-
-(function MyFunction() {
-    for(let i = 0; i < Mass.length; i++){
-        console.log(i);
-    }
-})(Mass);
-
-console.log(typeof st);*/
-
-let look = {
-    color: 'green',
-    jeans: "Moms",
-    changBottom:{
-        brand: 'Zara',
-        price:25
-    }
-};
-
-const B = {
-    first: "jopa",
-    two: "popa"
-};
-const A ={
-    first: "look",
-    third: "ppppp"
-};
-
-const END ={
-    ...B,
-    ...A
-};
-console.log(END);
