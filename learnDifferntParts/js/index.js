d742de3ddf5e2e2c5e55b392498807a15f1b0c0d
marinasaf777 <myjob.front.end@gmail.com>
2020-04-13 22:03:41 +0300

    accordeon

--- learnDifferntParts/js/index.js
+++ learnDifferntParts/js/index.js
@@ -1,32 +1,7 @@
 "use strict";
-/*for(let current= 20; ;current = current+1) if (current % 7 == 0) {
-    console.log(current);
-    break;
-};*/
-
 /*
-let point = "#";
-for(let current = 0; ;current++){
-    if(current==7){
-        break
-    }else{
-        point += "*";
-        console.log(point);
-    }
-}
-*/
-
-/*for(let i = 1; i <= 100; i++){
-    if(i%3 ==  0){
-        console.log("Buss");
-    }else if(i%5 == 0){
-        console.log("Riss")
-    }else{
-        console.log(i);
-    }
-}*/
 let Ozan = ['Ozan', 'Mehmet', 'Dolunay'];
 
 for (let parts in Ozan){
     console.log(Ozan[parts]);
-}
+}*/
