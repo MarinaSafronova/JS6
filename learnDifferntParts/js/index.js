fecff530917c1da92af8dc840e8522bb5c7f3b52
marinasaf777 <myjob.front.end@gmail.com>
2020-04-21 21:58:14 +0300

    add date

--- learnDifferntParts/js/index.js
+++ learnDifferntParts/js/index.js
@@ -4,13 +4,26 @@ let Ozan = ['Ozan', 'Mehmet', 'Dolunay'];
 const People =[
     {name: "Ozan", age: 29, budget: 2000},
     {name: "Uraz", age: 36, budget: 3000},
-    {name: "Cenk", age: 29, budget: 4000},
+    {name: "Cenk", age: 16, budget: 4000},
     {name: "Mert", age: 26, budget: 4000},
-    {name: "Ozan", age: 29, budget: 5200}
+    {name: "Ozan", age: 17, budget: 5200}
 ];
 
 People.forEach(function(item, i, arr){
-    console.log(item);
+   /* console.log(item);
     console.log(i);
-    console.log(arr);
+    console.log(arr);*/
 });
+
+const TwoMass = People.map(function (item) {
+    return item.age;
+});
+
+console.log(TwoMass);
+
+const AgeFilter = People.filter(function (item) {
+    if(item.age <=18){
+        return true;
+    }
+});
+console.log(AgeFilter);
--- todolist/js/index.js
+++ todolist/js/index.js
@@ -2,9 +2,27 @@
 const input = document.getElementById('text');
 const btnDelete = document.getElementById('btnDelete');
 const partLeft = document.getElementById('left-part');
+const element = document.querySelector('input[type=checkbox]');
+
+//Date optionst
+const date = document.getElementById('date-block');
+const options ={weekday: 'long', month:'short', day: 'numeric'};
+const today = new Date();
+date.innerHTML = today.toLocaleDateString('en-US', options);
+
 let list = [];
 let id = 0;
 
+function AddItem(text) {
+    let ItemLi = document.createElement('li');
+    let checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    ItemLi.classList.add('item');
+    checkBox.classList.add('typeCheck');
+    ItemLi.innerText = text;
+    ItemLi.appendChild(checkBox);
+    partLeft.appendChild(ItemLi);
+};
 
 input.addEventListener('keyup', function(event){
     if(event.keyCode == 13){
@@ -17,22 +35,22 @@ input.addEventListener('keyup', function(event){
                 done: false
             });
             id++;
-            console.log(list);
         }else{
             return false;
         }
     }
 });
 
-function AddItem(text) {
-    let ItemLi = document.createElement('li');
-    let ItemInput = document.createElement('input');
-    ItemInput.type = 'checkbox';
-    ItemLi.classList.add('item');
-    ItemLi.innerText = text;
-    ItemLi.appendChild(ItemInput);
-    partLeft.appendChild(ItemLi);
-};
+btnDelete.addEventListener('click', function(){
+    
+});
+function CompleteTask(chkBox) {
+    if (chkBox.checked) {
+        chkBox.className = "task completed";
+    } else {
+        /*this.incompleteTask(i);*/
+    }
+}
 
 
 
