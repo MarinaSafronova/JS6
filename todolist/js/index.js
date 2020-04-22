0694edfdea213f53800860fa8148a023a10f6896
marinasaf777 <myjob.front.end@gmail.com>
2020-04-22 23:47:07 +0300

    todo local storage

--- todolist/js/index.js
+++ todolist/js/index.js
@@ -1,10 +1,11 @@
 "use strict";
+//UI object
 const input = document.getElementById('text');
 const btnDelete = document.getElementById('btnDelete');
 const partLeft = document.getElementById('left-part');
-const element = document.querySelector('input[type=checkbox]');
 
-//Date optionst
+
+//Date options
 const date = document.getElementById('date-block');
 const options ={weekday: 'long', month:'short', day: 'numeric'};
 const today = new Date();
@@ -13,6 +14,7 @@ date.innerHTML = today.toLocaleDateString('en-US', options);
 let list = [];
 let id = 0;
 
+//Function Add item
 function AddItem(text) {
     let ItemLi = document.createElement('li');
     let checkBox = document.createElement('input');
@@ -24,33 +26,40 @@ function AddItem(text) {
     partLeft.appendChild(ItemLi);
 };
 
-input.addEventListener('keyup', function(event){
+//Event KeyUp
+input.addEventListener('keyup', function(){
     if(event.keyCode == 13){
         const ToDO = input.value.charAt(0).toUpperCase() + input.value.slice(1);
         if(ToDO){
             AddItem(ToDO);
-            list.push({
-                title: ToDO,
-                id: id,
-                done: false
-            });
+            list.push({title: ToDO, id: id, done: false});
             id++;
         }else{
             return false;
         }
+        input.value = "";
     }
 });
 
+/*
 btnDelete.addEventListener('click', function(){
-    
+
 });
+*/
+
+function BindTask() {
+    const checkBox = document.querySelector('input[type=checkbox]');
+
+}
+/*
 function CompleteTask(chkBox) {
     if (chkBox.checked) {
         chkBox.className = "task completed";
     } else {
-        /*this.incompleteTask(i);*/
+        /!*this.incompleteTask(i);*!/
     }
 }
+*/
 
 
 
