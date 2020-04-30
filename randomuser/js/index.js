08665c6cf21bba875b826636e8cbb60f778f9f62
marinasaf777 <myjob.front.end@gmail.com>
2020-04-30 20:12:48 +0300

    randomuser api done

--- randomuser/js/index.js
+++ randomuser/js/index.js
@@ -1,22 +1,49 @@
 "use strict";
 let ContainerUsers = document.getElementById('table-users');
-let Promise = fetch('https://randomuser.me/api/')
-    .then(data=>{
-        console.log(data);
-    });
+let request = new XMLHttpRequest();
 
 
-/*
-function PartView() {
-    let request = new XMLHttpRequest();
-    request.open({
-        url: 'https://randomuser.me/api/',
-        dataType: 'json',
-        success: function(data) {
-            console.log(data);
-        }
-    });
-    request.send();
+request.onreadystatechange  = function () {
+    if(this.readyState == 4 && this.status == 200){
+        FuncResponse(this.responseText);
+    }
+};
+request.open('GET', 'https://randomuser.me/api/?results=5');
+request.send();
+
+function FuncResponse(data) {
+    let ItemData = JSON.parse(data);
+    for(let key in ItemData){
+        ItemData[key].forEach(function (item, index, arr) {
+            CreateItem(item);
+        })
+    }
 }
 
-*/
+function CreateItem(item) {
+    let DivTemplate = document.createElement("div");
+    DivTemplate.classList.add('list-style')
+    let Template = ` <div class="row justify-content-center">
+                    <div class="col-lg-6 text-center">
+                        <div class="UserImg">
+                        <img src="${item.picture.large}" alt="User"/>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <ul>
+                            <li>Name:
+                                <span>${item.name.first}</span>
+                                <span>${item.name.last}</span>
+                            </li>
+                            <li>Age:   ${item.dob.age} years</li>
+                            <li>Phone: ${item.phone}</li>
+                            <li>Email: ${item.email}</li>
+                            <li>City:  ${item.location.city}</li>
+                        </ul>
+                    </div>
+                </div>
+    `;
+    DivTemplate.innerHTML = Template;
+    ContainerUsers.append(DivTemplate);
+}
+
